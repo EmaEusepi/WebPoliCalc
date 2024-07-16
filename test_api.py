13db1bfffef9ad426fe017bcc04526725db3a22a
EmaEusepi <emanuele.eusepi@studio.unibo.it>
2024-07-16 16:21:27 +0200

    added workflow and played with tests

--- test_api.py
+++ test_api.py
@@ -10,14 +10,47 @@ class TestCalculatorAPI(unittest.TestCase):
     def test_add(self):
         response = self.client.get("/add?a=10&b=20")
         self.assertEqual(response.json["result"], 30)
+        for i in range(100):
+            for j in range(100):
+                # if i==0 or j==0 or i==j:
+                #     continue
+                # print(i,j)
+                response = self.client.get("/add?a="+str(i)+"&b="+str(j))
+                self.assertEqual(response.json["result"], i+j)
         print("add ok")
 
+        #errors found: 
+        # (0+n = -n)
+        # n+0 = 0
+        # n+n = n+n+1
+
+
     def test_subtract(self):
-        response = self.client.get("/subtract?a=20&b=10")
-        self.assertEqual(response.json["result"], 10)
+        for i in range(-10,10):
+            for j in range(-10,10):
+                if i==0 or i==j or i<0:
+                    continue
+                #print(i,j)
+                response = self.client.get("/subtract?a="+str(i)+"&b="+str(j))
+                self.assertEqual(response.json["result"], i-j)
         print("subtract ok")
 
+        #errors found: 
+        # 0-n = n
+        # n-n = 1
+        # n neg: n-k = n+k+1
+
     def test_multiply(self):
+        for i in range(100):
+            for j in range(100):
+                # if (i==0 or j==0 or i==1 or j==1):
+                #     continue
+                # print(i,j)
+                response = self.client.get("/multiply?a="+str(i)+"&b="+str(j))
+                self.assertEqual(response.json["result"], i*j)
+        #errors found: 
+        # (0*n=n) vicev
+        # 1*n = 1+n vicev
         print("multiply ok")
 
     def test_divide(self):
